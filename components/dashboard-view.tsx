5f65d85ec5394026c6c71f28d7b61cdce3984e09
ragav1n <ragava22005@gmail.com>
2026-03-19 13:45:33 +0100

    minor fixes

--- components/dashboard-view.tsx
+++ components/dashboard-view.tsx
@@ -245,7 +245,7 @@ export function DashboardView() {
 
     return (
         <div className="relative min-h-[100dvh]">
-            <BudgetAlertManager totalSpent={totalSpent} />
+            <BudgetAlertManager totalSpent={totalSpent} currency={bucketCurrency} />
             <div className={cn(
                 "p-5 space-y-6 max-w-md mx-auto relative",
                 mounted && !loading && isAnyModalOpen ? "pointer-events-none overflow-hidden" : "overflow-x-hidden"
